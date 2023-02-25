f680bbc9402d806fb896c1ef768b011aea877b8c
XiangyuHuHu <huxiangyu99@163.com>
2023-02-25 15:14:49 -0500

    new database

--- src/main/resources/parkinglot.sql
+++ src/main/resources/parkinglot.sql
@@ -5,53 +5,74 @@
  Source Server Type    : MySQL
  Source Server Version : 50740 (5.7.40-log)
  Source Host           : localhost:3306
- Source Schema         : parking
+ Source Schema         : parkinglot
 
  Target Server Type    : MySQL
  Target Server Version : 50740 (5.7.40-log)
  File Encoding         : 65001
 
- Date: 20/02/2023 21:13:22
+ Date: 25/02/2023 15:12:05
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+-- ----------------------------
+-- Table structure for parkforall
+-- ----------------------------
+DROP TABLE IF EXISTS `parkforall`;
+CREATE TABLE `parkforall`  (
+  `id` int(11) NOT NULL,
+  `cardNum` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `parkNum` int(11) NULL DEFAULT NULL,
+  `plate` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `parkIn` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `parkOut` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci;
+
+-- ----------------------------
+-- Records of parkforall
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for parkinfo
+-- ----------------------------
+DROP TABLE IF EXISTS `parkinfo`;
+CREATE TABLE `parkinfo`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `park_num` int(11) NULL DEFAULT NULL,
+  `card_num` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `plate` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `entrance` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+
+-- ----------------------------
+-- Records of parkinfo
+-- ----------------------------
+BEGIN;
+INSERT INTO `parkinfo` (`id`, `park_num`, `card_num`, `plate`, `entrance`) VALUES (1, 0, NULL, '123456', '2023-02-24 18:22:44');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` int(11) NOT NULL auto_increment,
-  `username` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  'role' int(11) default '3' COMMENT '1: superadmin, 2: admin, 3: user',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `role` int(11) NULL DEFAULT 3 COMMENT '1: super admin, 2: admin, 3: user',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 BEGIN;
-INSERT INTO `user` ('id', 'username', 'password', 'role') VALUES ('0', 'admin', 'admin', '1');
+INSERT INTO `user` (`id`, `username`, `password`, `role`) VALUES (1, 'admin', 'admin', 1);
 COMMIT;
-
--- ----------------------------
--- Table structure for `parkinfo`
--- ----------------------------
-DROP TABLE IF EXISTS `parkinfo`;
-CREATE TABLE `parkinfo` (
-    `id` int(11) NOT NULL auto_increment,
-    `parkNum` int(11) default NULL,
-    `cardNum` varchar(50) default NULL,
-    `plate` varchar(50) default NULL ,
-    `entrance` datetime default NULL,
-    PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of parkinfo
--- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
