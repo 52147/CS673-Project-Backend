8c2f6569bb00d149ebf1d054fbea63a674f60d13
Chenstonist <chenstc@bu.edu>
2023-03-01 16:42:03 -0500

    authority functionality

---
+++ src/main/java/com/cs673/backend/controller/UserController.java
@@ -0,0 +1,96 @@
+package com.cs673.backend.controller;
+
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.cs673.backend.entity.User;
+import com.cs673.backend.service.UserService;
+import org.springframework.web.bind.annotation.*;
+import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.util.List;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Resource
+    private UserService userService;
+
+    // 新增或者更新
+    @PostMapping
+    public boolean save(@RequestBody User user) {
+        return userService.saveOrUpdate(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public Boolean delete(@PathVariable Integer id) {
+        return userService.removeById(id);
+    }
+
+    @PostMapping("/del/batch")
+    public boolean deleteBatch(@RequestBody List<Integer> ids) {
+        return userService.removeByIds(ids);
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        return userService.list();
+    }
+
+    @GetMapping("/{id}")
+    public User findOne(@PathVariable Integer id) {
+        return userService.getById(id);
+    }
+
+    @GetMapping("/page")
+    public IPage<User> findPage(@RequestParam Integer pageNum,
+                                @RequestParam Integer pageSize,
+                                @RequestParam(defaultValue = "") String username,
+                                @RequestParam(defaultValue = "") Integer role) {
+        IPage<User> page = new Page<>(pageNum, pageSize);
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        if (!"".equals(username)) {
+            queryWrapper.like("username", username);
+        }
+        if (!"".equals(role)) {
+            queryWrapper.like("role", role);
+        }
+        //queryWrapper.orderByDesc("id");
+        return userService.page(page, queryWrapper);
+    }
+
+    @GetMapping("/export")
+    public void export(HttpServletResponse response) throws Exception {
+        List<User> list = userService.list();
+        ExcelWriter writer = ExcelUtil.getWriter(true);
+        writer.addHeaderAlias("username", "用户名");
+        writer.addHeaderAlias("password", "密码");
+        writer.write(list, true);
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        String fileName = URLEncoder.encode("用户信息", "UTF-8");
+        response.setHeader("Content-Disposition", "attachment;filename=" + fileName + ".xlsx");
+        ServletOutputStream out = response.getOutputStream();
+        writer.flush(out, true);
+        out.close();
+        writer.close();
+
+    }
+    @PostMapping("/import")
+    public Boolean imp(MultipartFile file) throws Exception {
+        InputStream inputStream = file.getInputStream();
+        ExcelReader reader = ExcelUtil.getReader(inputStream);
+        List<User> list = reader.readAll(User.class);
+        userService.saveBatch(list);
+        return true;
+    }
+
+}
--- src/main/java/com/cs673/backend/serviceImp/UserServiceImp.java
+++ src/main/java/com/cs673/backend/serviceImp/UserServiceImp.java
@@ -1,16 +1,22 @@
 package com.cs673.backend.serviceImp;
 
+import cn.hutool.log.Log;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cs673.backend.DTO.UserDTO;
 import com.cs673.backend.entity.User;
+import com.cs673.backend.mapper.UserMapper;
 import com.cs673.backend.repository.UserRepository;
 import com.cs673.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImp implements UserService {
-
+public class UserServiceImp extends ServiceImpl<UserMapper, User>  implements UserService {
+    private static final Log LOG = Log.get();
     @Autowired
     private UserRepository userRepository;
+
     @Override
     public User findUserByUsername(String username) {
         return userRepository.findByUsername(username);
@@ -20,4 +26,5 @@ public class UserServiceImp implements UserService {
     public User findByUsernameAndPassword(String username, String password) {
         return userRepository.findByUsernameAndPassword(username, password);
     }
+
 }
