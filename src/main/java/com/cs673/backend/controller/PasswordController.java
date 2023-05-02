3873f2c71f797ea8de6e2be291360cc67876207a
XiangyuHuHu <huxiangyu99@163.com>
2023-05-01 22:34:00 -0400

    add password and login test

--- src/main/java/com/cs673/backend/controller/PasswordController.java
+++ src/main/java/com/cs673/backend/controller/PasswordController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:3000")
 public class PasswordController {
     @Autowired
-    private UserService userService;
+    UserService userService;
 
     @RequestMapping("/forget")
     @PostMapping("")
